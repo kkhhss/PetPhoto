0d5f6423df21cff7900de4ca398d452d1d8d3fb0
kkhhss <yg91238405@gmail.com>
2023-05-18 20:05:33 +0900

    레이아웃 제작, 변수명 배당, 체크박스 실행

--- app/src/main/java/kr/hs/emirim/kkhhss/petphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/petphoto/MainActivity.kt
@@ -1,11 +1,37 @@
 package kr.hs.emirim.kkhhss.petphoto
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var checkStart:CheckBox
+    lateinit var rg:RadioGroup
+    lateinit var linear:LinearLayout
+    lateinit var btnDone:Button
+    lateinit var imgv: ImageView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkStart = findViewById(R.id.check_start)
+        rg = findViewById(R.id.rg)
+        linear = findViewById(R.id.linear)
+        btnDone = findViewById(R.id.btn_done)
+        imgv = findViewById(R.id.imgv)
+        linear.visibility = View.INVISIBLE
+
+        checkStart.setOnCheckedChangeListener{compoundButton, b ->
+            if(checkStart.isChecked){
+                linear.visibility = View.VISIBLE
+            }
+            else{
+                linear.visibility = View.INVISIBLE
+            }
+        }
+
     }
 }
