e9356d5ba12c9698ffbcdaa8587ed0ef8e0fab44
kkhhss <yg91238405@gmail.com>
2023-05-23 20:00:27 +0900

    종료, 초기화 버튼 추가

--- app/src/main/java/kr/hs/emirim/kkhhss/petphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/petphoto/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var rg:RadioGroup
     lateinit var linear:LinearLayout
     lateinit var imgv: ImageView
+    lateinit var btnFinish:Button
+    lateinit var btnFirst:Button
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
         imgv = findViewById(R.id.imgv)
+        btnFinish=findViewById(R.id.btn_finish)
+        btnFirst=findViewById(R.id.btn_first)
         linear.visibility = View.INVISIBLE
 
         checkStart.setOnCheckedChangeListener{compoundButton, b ->
@@ -36,9 +41,18 @@ class MainActivity : AppCompatActivity() {
                 R.id.radio_dog -> imgv.setImageResource(R.drawable.dog)
                 R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
                 R.id.radio_ham -> imgv.setImageResource(R.drawable.hamster)
-                else -> Toast.makeText(applicationContext, "라디오버튼이 선택되지 않았습니다.", Toast.LENGTH_SHORT).show()
             }
         }
 
+        btnFinish.setOnClickListener(btnListener)
+        btnFirst.setOnClickListener(btnListener)
+
+    }
+
+    var btnListener = OnClickListener{
+        when(it.id) {
+            R.id.btn_finish -> finish()
+            R.id.btn_first -> linear.visibility = View.INVISIBLE
+        }
     }
 }
